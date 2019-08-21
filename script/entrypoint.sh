733bfd34ebe16c471988b95bb158fcfc61a1ad51
Mihai Nechita <mihai.nechita@endava.com>
2019-08-21 22:57:06 +0300

    change executor to LocalExecutor

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -15,7 +15,7 @@ TRY_LOOP="20"
 # Defaults and back-compat
 : "${AIRFLOW_HOME:="/usr/local/airflow"}"
 : "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
-: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
+: "${AIRFLOW__CORE__EXECUTOR:="LocalExecutor"}"
 
 export \
   AIRFLOW_HOME \
